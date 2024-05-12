db28660d67bb4293446daaa08deb78a53fa7a5f2
printhapan <printhapan@gmail.com>
2024-05-12 12:58:22 +0530

    changed the code

--- colorbeam_test.py
+++ colorbeam_test.py
@@ -10,15 +10,15 @@ json.encoder.encode_basestring_ascii(command)
 
 async def main(host,port,command):
     try:
-        writer , reader = await asyncio.open_connection(host= host,port= port)
+        reader,writer = await asyncio.open_connection(host= host,port= port)
         print( "connected to ({host}:{port})")
         writer.write("{command}\n".encode())
         data = await reader.read(10000)
         formated = json.dumps(data,ident=4)
         print(formated)
         writer.close()
+        await writer.wait_closed()
     except Exception as e :
         raise Exception("ERROR:%s", e)
     
-
 asyncio.run(main(host,port,command))
