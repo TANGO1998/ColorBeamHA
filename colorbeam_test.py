4eb97eb93f73583a6632a3f8ab968b5ea832b678
TANGO1998 <printhapan@gmail.com>
2024-05-13 15:22:18 +0530

    changed the code

--- colorbeam_test.py
+++ colorbeam_test.py
@@ -5,7 +5,7 @@ import json
 LOGGER = logging.getLogger(__name__)
 host = "192.168.8.172"
 port = "3334"
-command = {"command":"SetLoads", "params":[{"id":180,"d":750,"l":0}]}
+command = {"command":"SetLoads", "params":[{"id":180,"d":1000,"l":0},{"id":179,"d":1000,"l":0}]}
 
 async def send_json_via_telnet(host, port, json_data):
 
@@ -13,7 +13,7 @@ async def send_json_via_telnet(host, port, json_data):
 
     data_to_send = json.dumps(json_data).encode('utf-8')
     writer.write(data_to_send + b'\n') 
-    data = await reader.read(-1) 
+    data = await reader.read(10000) 
 
     writer.close()
 
