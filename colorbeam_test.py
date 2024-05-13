73dca88e997fb713a87866d42f849fe6fb1295da
printhapan <printhapan@gmail.com>
2024-05-13 23:50:24 +0530

    changed the code

--- colorbeam_test.py
+++ colorbeam_test.py
@@ -5,11 +5,12 @@ import json
 LOGGER = logging.getLogger(__name__)
 host = "192.168.8.172"
 port = "3334"
-command = {"command":"SetLoads", "params":[{"id":180,"d":1000,"l":0},{"id":179,"d":1000,"l":0}]}
+command = {"command":"GetLoadStatus"}
 
 async def send_json_via_telnet(host, port, json_data):
 
     reader, writer = await asyncio.open_connection(host, port)
+    print(f"connected to {host} : {port}")
 
     data_to_send = json.dumps(json_data).encode('utf-8')
     writer.write(data_to_send + b'\n') 
@@ -18,7 +19,7 @@ async def send_json_via_telnet(host, port, json_data):
     writer.close()
 
     if data:
-        print(f"Received response from server: {data.decode('utf-8')}")
+        print(f"Received response from server: {json.dump(data.decode('utf-8'))}")
     else:
         print("No response received from server.")
     
