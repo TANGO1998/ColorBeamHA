875ac1c11798613cd3b822249694e1b506855d38
TANGO1998 <printhapan@gmail.com>
2024-08-16 18:57:44 +0530

    added async_setup_entry

--- __init__.py
+++ __init__.py
@@ -4,12 +4,12 @@ from dataclasses import dataclass
 import logging
 
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import CONF_HOST, CONF_PASSWORD, CONF_USERNAME, Platform
+from homeassistant.const import CONF_HOST,CONF_PORT, Platform
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import device_registry as dr, entity_registry as er
 
 from .const import DOMAIN
-
+from .pycolorbeam import ColorBeamBaseInstance,ColorBeamLightInstance,ColorBeamRGBLightInstance
 PLATFORMS = [
     Platform.LIGHT,
 ]
@@ -24,3 +24,24 @@ class ColorBeamData:
     """Storage class for platform global data."""
     lights: list[tuple[str, Output]]
 
+async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
+    """Set up the ColorBeam integration."""
+
+    host = config_entry.data[CONF_HOST]
+    port = config_entry.data[CONF_PORT]
+
+
+    colorbeam_client = ColorBeamBaseInstance(host,port)
+    _LOGGER.info("connecting to gateway")
+    RGB , BI = await hass.async_add_executor_job(colorbeam_client.updateall())
+    
+    entity_registry = er.async_get(hass)
+    device_registry = dr.async_get(hass)
+
+    _LOGGER.info("adding devices")
+    
+    entry_data = ColorBeamData(
+        lights = []
+    )
+
+    
