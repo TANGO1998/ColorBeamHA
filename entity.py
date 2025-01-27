67934c29fbf9ad0593624e6b676a5e8e3a0cb6ee
TANGO1998 <printhapan@gmail.com>
2025-01-27 13:15:16 +0530

    for future update entity creation is planned

--- entity.py
+++ entity.py
@@ -1,61 +1,25 @@
 """Base class for ColorBeam devices."""
-
-from pycolorbeam import ColorBeamBaseInstance,ColorBeamLightInstance,ColorBeamRGBLightInstance
-
-from homeassistant.const import ATTR_IDENTIFIERS, ATTR_VIA_DEVICE
+from homeassistant.helpers import device_registry as dr
 from homeassistant.helpers.device_registry import DeviceInfo
-from homeassistant.helpers.entity import Entity
-from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import CONF_HOST,CONF_PORT
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
 from .const import DOMAIN
+from .coordinator import ColorBeamUpdateCoordinator
 
-class ColorBeamBaseEntity(Entity):
+class ColorBeamBaseEntity(CoordinatorEntity):
     """Base class for ColorBeam entities."""
 
     _attr_should_poll = True
     _attr_has_entity_name = True
 
-    def __init__(
-            self,ColorBeamDeviceUuid: ConfigEntry.entry_id
-    )-> None:
-        """initalize the device."""
-        self._uuid = ColorBeamDeviceUuid
-
-    async def async_added_to_hass(self) -> None:
-        """Register callbacks."""
-        
-
-    def _request_state(self) -> None:
-        """Request the state."""
-
-    def _update_attrs(self) -> None:
-        """Update the entity's attributes."""
-
-    def _update_callback(
-            self,_context: None,_params:dict
-    )-> None:
-        """RUn when invoked by pycolorbeam when the device state changes."""
-        self._update_attrs()
-        self.schedule_update_ha_state()
-    
     @property
-    def unique_id(self)->str:
-        """Return a unique ID."""
-
-        return self._uuid
+    def deviceInfo(self)->DeviceInfo:
+        """Return device Info"""
+        version = self.coordinator.version
+        return DeviceInfo(
+            identifiers={(DOMAIN,"B8:27:EB:5A:56:B0")},
+            name="colorbeam light",
+            manufacturer="ColorBeam",
+            sw_version=version,
+        )
     
-    def update(self)->None:
-        """Update the entity's state."""
-        self._request_state()
-        self._update_attrs()
-
-class ColorBeamLightDevice(ColorBeamBaseEntity):
-    """Representation of a ColorBeam Light Device"""
-
-    def __init__(
-            self
-    )->None:
-
-
-        
