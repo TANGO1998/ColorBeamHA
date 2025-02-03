6032bf6f91917bc649ec90c19931c589fe42c4a4
printhapan <printhapan@gmail.com>
2025-02-03 10:47:41 +0530

    deleted the test files

--- colorbeam_test.py
+++
@@ -1,7 +0,0 @@
-from colorbeam/pycolorbeam import ColorBeamLightInstance,ColorBeamRGBLightInstance
-import asyncio
-import logging
-
-light = ColorBeamLightInstance("192.168.8.172","3334",180)
-asyncio.run(light.turn_on(0))
-print(f"light brightness: {light.Getbrightness}\n light brightness: {light.is_on}")
